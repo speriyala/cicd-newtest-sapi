22d76f8a85cb7c4729fc2be0866672de92800e32
speriyala <speriyala@open-logix.com>
2023-04-26 12:14:37 +0530

    new change in workflow name

---
+++ shyam.sh
@@ -0,0 +1,18 @@
+dir="git@github.com:speriyala/cicd-newtest-sapi.git";
+if [ ! -d "$dir" ] 
+then
+  echo -e "No Version Control History Found. Initializing the Current Directory \n";
+git init;
+flag=1;
+else
+  echo -e "\nVersion control history found. \n";
+fi
+echo -e "\nAdding Files to Push\n";
+git add .;
+echo -e "\nPrinting Git Status \n";
+git status;
+echo -e "\nCan I confirm the push boss (y/n) ?";
+read user_input;
+git commit -m "new change in workflow name";
+git push origin develop;
+echo;
