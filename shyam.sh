bf81c91572395c7593d0ced5bf6bc619957a191d
speriyala <speriyala@open-logix.com>
2023-04-27 14:06:04 +0530

    new commit

--- shyam.sh
+++ shyam.sh
@@ -1,18 +1,11 @@
-dir="git@github.com:speriyala/cicd-newtest-sapi.git";
-if [ ! -d "$dir" ] 
-then
-  echo -e "No Version Control History Found. Initializing the Current Directory \n";
-git init;
-flag=1;
-else
-  echo -e "\nVersion control history found. \n";
-fi
-echo -e "\nAdding Files to Push\n";
-git add .;
-echo -e "\nPrinting Git Status \n";
-git status;
-echo -e "\nCan I confirm the push boss (y/n) ?";
-read user_input;
-git commit -m "workflow-gitsteps-automate";
-git push origin develop;
-echo;
+pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+    }
+}
