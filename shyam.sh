a277bc7a914b4b6c6f47f300b7ca27c63045bd7f
speriyala <speriyala@open-logix.com>
2023-04-27 14:11:39 +0530

    new code

--- shyam.sh
+++ shyam.sh
@@ -1,11 +1,29 @@
 pipeline {
     agent any
 
+    tools {
+        // Install the Maven version configured as "M3" and add it to the path.
+        maven "M3"
+    }
+
     stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello World'
+        stage('Build') 
+        
+        {
+            steps 
+                {
+                // Get some code from a GitHub repository
+                git 'https://github.com/speriyala/cicd-newtest-sapi.git'
+
+                // Run Maven on a Unix agent.
+                sh "mvn -Dmaven.test.failure.ignore=true clean package"
+
+                
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
+
         }
     }
 }
